dbbed2aa4e054fd179e15d314f3a364b90ba0528
imran2mna <imranen08@gmail.com>
2017-01-22 22:26:42 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name="HinguloyaWS",
+      version='1.0.3.1',
+      description="Hinguloya web server components",
+      author="Imran Mohamed",
+      author_email="imranen08@gmail.com",
+      url="",
+      packages=['conf', 'file', 'http', 'server']
+      )
