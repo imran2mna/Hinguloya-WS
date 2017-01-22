8387c254307acd69d69a07e677734deb1787ef57
imran2mna <imranen08@gmail.com>
2017-01-22 22:27:08 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name="HinguloyaWS",
-      version='1.0.3.1',
+      version='1.0',
       description="Hinguloya web server components",
       author="Imran Mohamed",
       author_email="imranen08@gmail.com",
