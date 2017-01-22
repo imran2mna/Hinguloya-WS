91a0012f834f3817ee5edbe99ae2ede25c01b34b
imran2mna <imranen08@gmail.com>
2017-01-22 22:23:12 +0530

    Add files via upload

--- conf/basic.py
+++ conf/basic.py
@@ -1,7 +1,8 @@
 SERVER_NAME = "Hinguloya WS"
-DOCUMENT_ROOT = "/home/imran/Desktop/"
-DEFAULT_FILE = "/index.html"
+DOCUMENT_ROOT = "/var/www/html/"
+DEFAULT_FILE = "index.html"
 INTERNAL_ERROR = "Internal server error :("
+SUPRESS_EXCEPTION = True
 
 BAD_REQ_FILE = "bad_request.html"
 NOT_FOUND_FILE = "not_found.html"
